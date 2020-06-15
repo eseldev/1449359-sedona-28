7b798eddc3529816769cca264a6174621bc6a358
seldeved <eseldev@yandex.ru>
2020-06-15 14:14:40 +0300

    Правки

--- js/hotel-search.js
+++ js/hotel-search.js
@@ -8,31 +8,33 @@ var kidsInput = searchPopup.querySelector(".kids-num-input");
 var searchForm = searchPopup.querySelector(".search-form");
 
 var isStorageSupport = true;
-var storage = "";
+var storage1 = "";
+var storage2 = "";
 
 try {
-  storage = localStorage.getItem("adults");
-  storage = localStorage.getItem("kids");
+    storage1 = localStorage.getItem("adults");
+    storage2 = localStorage.getItem("kids");
 } catch (err) {
-  isStorageSupport = false;
+    isStorageSupport = false;
 }
 
 searchButton.addEventListener("click", function (evt) {
     evt.preventDefault();
     searchPopup.classList.toggle("search-popup-hide");
-    if (storage) {
-        adultsInput.value = storage;
-    } else {
-        kidsInput.value = storage;
-      }
+    searchPopup.classList.remove("search-popup-error");
     arrivalInput.focus();
 });
 
 searchForm.addEventListener("submit", function (evt) {
     if (!arrivalInput.value || !departureInput.value || !adultsInput.value) {
         evt.preventDefault();
+        searchPopup.classList.remove("search-popup-error");
+        searchPopup.offsetWidth = searchPopup.offsetWidth;
+        searchPopup.classList.add("search-popup-error");
     } else {
-        localStorage.setItem("adults", adultsInput.value);
-        localStorage.setItem("kids", kidsInput.value);
-      }
+        if (isStorageSupport) {
+            localStorage.setItem("adults", adultsInput.value);
+            localStorage.setItem("kids", kidsInput.value);
+        }
+    }
 });
